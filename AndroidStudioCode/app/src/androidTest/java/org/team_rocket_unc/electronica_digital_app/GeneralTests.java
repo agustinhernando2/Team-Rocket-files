c87c894e8db9df3c041ffcf1c8a8089c321ac8a6
Icel <icel.vinals@gmail.com>
2022-05-22 14:27:50 -0300

    Added a versionTest

---
+++ AndroidStudioCode/app/src/androidTest/java/org/team_rocket_unc/electronica_digital_app/GeneralTests.java
@@ -0,0 +1,24 @@
+package org.team_rocket_unc.electronica_digital_app;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class GeneralTests {
+
+    @Test
+    public void testVersion() {
+        int EXPECTED_MIN_SDK_VERSION = 21;
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Assert.assertEquals(EXPECTED_MIN_SDK_VERSION, appContext.getApplicationInfo().minSdkVersion);
+    }
+
+
+
+}
