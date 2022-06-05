53caa74783921d1cb132b6b9f436b197306c5334
Camila <careggiocamila@gmail.com>
2022-06-05 12:35:28 -0300

    Unit Tests

---
+++ AndroidStudioCode/app/src/test/java/org/team_rocket_unc/electronica_digital_app/CalculatorTests/Led.java
@@ -0,0 +1,31 @@
+package org.team_rocket_unc.electronica_digital_app.CalculatorTests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.team_rocket_unc.electronica_digital_app.units.unit_1_calculators.p3_led.LedCircuitToolModel;
+
+public class Led {
+    LedCircuitToolModel staticLed = new LedCircuitToolModel();
+
+    @Test
+    public void calculateResistance() {
+        staticLed.setLedTension(2);
+        staticLed.calculate("0.02", "5");
+        assertEquals("130", staticLed.getResistance());
+    }
+
+    @Test
+    public void testNegativeInput() {
+        staticLed.setLedTension(0);
+        staticLed.calculate("-0.015", "3");
+        assertEquals("-73", staticLed.getResistance());
+    }
+
+    @Test
+    public void checkColorSelector() {
+        staticLed.setLedTension(4);
+        assertEquals("3.4", staticLed.getLedTension());
+    }
+
+}
