6135e087c2ea24f010039a0cb35172e750eba695
Camila <careggiocamila@gmail.com>
2022-06-06 20:44:38 -0300

    Mejoras?
    Ahora hace algo :+1:  pero falta:
    1. Limitar la cantidad de checkboxes  a 2 pulsados
    2. Agregar imagen y acomodar layout
    3. Arreglar que se rompe todo cuando pongo algunos valores negativos (revisar en raiz cuadrada del Model) :-1:
    4. Poner lindo el código

--- AndroidStudioCode/app/src/main/java/org/team_rocket_unc/electronica_digital_app/units/unit_1_calculators/p2_ohm/OhmToolFragment.java
+++ AndroidStudioCode/app/src/main/java/org/team_rocket_unc/electronica_digital_app/units/unit_1_calculators/p2_ohm/OhmToolFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -13,7 +15,13 @@ import androidx.fragment.app.Fragment;
 import org.team_rocket_unc.electronica_digital_app.R;
 import org.team_rocket_unc.electronica_digital_app.utils.KeyboardUtils;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class OhmToolFragment extends Fragment {
+
+    public List<Boolean> selectedInputs = Arrays.asList(false,false,false,false);
 
     View view;
     EditText resistance;
@@ -21,6 +29,10 @@ public class OhmToolFragment extends Fragment {
     EditText power;
     EditText current;
     OhmToolModel model;
+    CheckBox boxR;
+    CheckBox boxT;
+    CheckBox boxP;
+    CheckBox boxC;
 
     @Nullable
     @Override
@@ -37,6 +49,11 @@ public class OhmToolFragment extends Fragment {
         tension = view.findViewById(R.id.tensionInput);
         power = view.findViewById(R.id.powerInput);
         current = view.findViewById(R.id.intensityInput);
+
+        boxC = view.findViewById(R.id.checkBoxI);
+        boxP=view.findViewById(R.id.checkBoxP);
+        boxT = view.findViewById(R.id.checkBoxV);
+        boxR=view.findViewById(R.id.checkBoxR);
     }
 
     private void setUpUI(){
@@ -45,19 +62,98 @@ public class OhmToolFragment extends Fragment {
         KeyboardUtils.closeKeyboardOnEnter(current,getActivity());
         KeyboardUtils.closeKeyboardOnEnter(power,getActivity());
 
-        KeyboardUtils.functionAfterChange(resistance,() ->{
-            model.setResistance(this.resistance.getText().toString());
+        boxR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedInputs.set(0, !selectedInputs.get(0));
+            }
         });
-        KeyboardUtils.functionAfterChange(tension,() ->{
-            model.setTension(this.tension.getText().toString());
+        boxT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedInputs.set(1, !selectedInputs.get(1));
+            }
         });
-        KeyboardUtils.functionAfterChange(current,() ->{
-            model.setCurrent(this.current.getText().toString());
+        boxC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedInputs.set(2, !selectedInputs.get(2));
+            }
         });
-        KeyboardUtils.functionAfterChange(power,() ->{
-            model.setPower(this.power.getText().toString());
+        boxP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedInputs.set(3, !selectedInputs.get(3));
+            }
         });
 
+        KeyboardUtils.functionAfterChange(resistance,() ->{
+            if(boxR.isChecked()){
+                String input = this.resistance.getText().toString();
+                model.setResistance(input.length() > 0 ? input : "0");
+                //selectedInputs.set(0,true);
+                updateOutputs();
+            } else {
+                model.setResistance("0");
+                //selectedInputs.set(0,false);
+            }
+        });
+
+        KeyboardUtils.functionAfterChange(tension,() ->{
+            if(boxT.isChecked()){
+                String input = this.tension.getText().toString();
+                model.setTension(input.length() > 0 ? input : "0");
+                //selectedInputs.set(1,true);
+                updateOutputs();
+            } else {
+                model.setTension("0");
+                //selectedInputs.set(1,false);
+            }
+            System.out.print(selectedInputs);
+        });
+
+        KeyboardUtils.functionAfterChange(current,() ->{
+            if(boxC.isChecked()){
+                String input = this.current.getText().toString();
+                model.setCurrent(input.length() > 0 ? input : "0");
+                //selectedInputs.set(2,true);
+                updateOutputs();
+            } else {
+                model.setCurrent("0");
+                //selectedInputs.set(2,false);
+            }
+
+        });
+        KeyboardUtils.functionAfterChange(power,() ->{
+            if(boxP.isChecked()){
+                String input = this.power.getText().toString();
+                model.setPower(input.length() > 0 ? input : "0");
+                //selectedInputs.set(3,true);
+                updateOutputs();
+            } else {model.setPower("0");
+                //selectedInputs.set(3,false);
+                }
+        });
+    }
+
+    private void updateOutputs(){
+        System.out.println(Collections.frequency(selectedInputs,true));
+        if(Collections.frequency(selectedInputs,true)==2){
+            model.calculateAll(selectedInputs);
+            if(selectedInputs.get(0)){ //mismo orden y logica que el calculateAll del Model
+                if(selectedInputs.get(1)){current.setText(model.getCurrent()); power.setText(model.getPower());}
+                else if(selectedInputs.get(2)){tension.setText(model.getTension()); power.setText(model.getPower());}
+                else if(selectedInputs.get(3)){tension.setText(model.getTension()); current.setText(model.getCurrent());}
+            }
+            else if(selectedInputs.get(1)){
+                if(selectedInputs.get(2)){resistance.setText(model.getResistance()); power.setText(model.getPower());}
+                else if(selectedInputs.get(3)){resistance.setText(model.getResistance()); current.setText(model.getCurrent());}
+            }
+            else if(selectedInputs.get(2) && selectedInputs.get(3)){
+                resistance.setText(model.getResistance());
+                tension.setText(model.getTension());
+            }
+        }
     }
 
 }
--- AndroidStudioCode/app/src/main/java/org/team_rocket_unc/electronica_digital_app/units/unit_1_calculators/p2_ohm/OhmToolModel.java
+++ AndroidStudioCode/app/src/main/java/org/team_rocket_unc/electronica_digital_app/units/unit_1_calculators/p2_ohm/OhmToolModel.java
@@ -1,42 +1,51 @@
 package org.team_rocket_unc.electronica_digital_app.units.unit_1_calculators.p2_ohm;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class OhmToolModel {
+
     private double tension=0;
     private double resistance=0;
     private double current=0;
     private double power=0;
 
-    public void setTension(String source){
-        tension=Double.parseDouble(source);
-    }
+    public void setCurrent(String intensity){ current=Double.parseDouble(intensity); }
+    public void setTension(String source){ tension=Double.parseDouble(source); }
     public void setResistance(String resistor){
         resistance=Double.parseDouble(resistor);
-    }
-    public void setCurrent(String intensity){
-        current=Double.parseDouble(intensity);
     }
     public void setPower(String pow){
         power=Double.parseDouble(pow);
     }
 
     public String getTension(){
-        return Double.toString(tension);
+        return String.format("%.2f", tension);
     }
     public String getCurrent(){
-        return Double.toString(current);
+        return String.format("%.2f", current);
     }
     public String getResistance(){
-        return Double.toString(resistance);
+        return String.format("%.2f", resistance);
     }
-    public String getPower(){
-        return Double.toString(power);
-    }
+    public String getPower(){return String.format("%.2f", power);}
 
-    public void calculateTension(){
-        tension= ((current!=0 && resistance!=0) ? 0 : current*resistance);
-        if(tension==0){
-            if(resistance==0){tension=power/current;}
-            else{tension=Math.sqrt(power/resistance);}
+    public void calculateAll(List<Boolean> selected){
+        if(Collections.frequency(selected, true)==2){
+            if(selected.get(0)){ // resistencia seleccionada
+                if(selected.get(1)){current=tension/resistance; power=Math.pow(tension,2)/resistance;} //tension seleccionada
+                else if(selected.get(2)){tension=current*resistance; power=Math.pow(current,2)*resistance;} // corriente seleccionada
+                else if(selected.get(3)){tension=Math.sqrt(power*resistance); current=Math.sqrt(power/resistance);} // power seleccionada
+            }
+            else if(selected.get(1)){ // tension seleccionada
+                if(selected.get(2)){resistance=tension/current; power=tension*current;} // corriente seleccionada
+                else if(selected.get(3)){resistance=Math.pow(tension,2)/power; current=power/tension;} // power seleccionda
+            }
+            else if(selected.get(2) && selected.get(3)){ // corriente y power
+                resistance=power/Math.pow(current,2);
+                tension=power/current;
+            }
         }
     }
 }
